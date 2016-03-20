185887f629d005a20e9c239f59aaeaa140c8fd5b
xmubaga <xmubaga@xmubaga-PC>
2016-03-20 11:12:39 +0800

    修改启动器，将流的关闭放入finally中

--- src/webServer/Controller.java
+++ src/webServer/Controller.java
@@ -3,15 +3,22 @@ package webServer;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-
+/**
+ * 该类用于启动或停止服务器
+ * @author xmubaga
+ *
+ */
 public class Controller {
-
+	/**
+	 * 控制器自启动main方法
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String command = null;
 		HttpServer httpServer = null;
-		while (true) {
-			try {
+		try {
+			while (true) {
 				command = reader.readLine();
 				if (command.equals("start")) {
 					if (httpServer == null) {
@@ -20,7 +27,6 @@ public class Controller {
 						System.out.println("服务器已经启动，请勿重复此操作！");
 					}
 				}
-				command = reader.readLine();
 				if (command.equals("exit")) {
 					if (httpServer != null) {
 						// 释放资源
@@ -29,19 +35,21 @@ public class Controller {
 					// 等待已有任务结束，退出
 					break;
 				}
-
-			} catch (IOException e) {
-				e.printStackTrace();
+			}
+			System.out.println("服务器停止了...");
+		} catch (IOException e) {
+			//thow new IOException("控制台输入异常");
+			//日子记录异常
+		}finally{
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
-		System.out.println("服务器停止了...");
-		if (reader != null) {
-			try {
-				reader.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
+		
 	}
 
 }
